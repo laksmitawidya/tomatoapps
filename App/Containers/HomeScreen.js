52480de7669bf8308d24c856b364f4e15381d647
Laksmita <mithawid@gmail.com>
2017-12-12 09:30:41 +0700

    Update HomeScreen.js

--- App/Containers/HomeScreen.js
+++ App/Containers/HomeScreen.js
@@ -9,30 +9,32 @@ import style from '../Components/Styles/TomatoStyles'
 import {connect} from 'react-redux'
 import TomatoActions from '../Redux/TomatoRedux'
 import { Item, Picker, Body, Container, Header, Content, Card, CardItem, Text, Icon, Right } from 'native-base';
+import { Icon as IconTwo } from 'react-native-elements';
 import Config from '../Config/AppConfig'
 
 class HomeScreen extends Component {
   constructor (props) {
     super(props)
-    const ds = new ListView.DataSource({ 
+    const ds = new ListView.DataSource({
       rowHasChanged: (r1, r2) => r1 !== r2 });
     this.state = {
       categories: [],
       categoryName:'',
       categoryId: 0,
       dataSource: ds.cloneWithRows([]),
-      selected: 280
+      selected: "280"
     }
-    
+
   }
-  
+
   onValueChange(value) {
     this.setState({
       selected: value
     });
+    this.props.switchEntity(value)
   }
 
-  
+
   setupCategories () {
     if (!this.props.categoriesPayload) {
       this.props.categoriesRequest()
@@ -41,6 +43,13 @@ class HomeScreen extends Component {
         categories: this.props.categoriesPayload.categories,
         dataSource: this.state.dataSource.cloneWithRows(this.props.categoriesPayload.categories)
       })
+
+      // check the entity id is changed or not from city picker list
+      if(this.props.entity_id){
+        this.setState({
+          selected: this.props.entity_id.toString(),
+        })
+      }
     }
   }
 
@@ -53,11 +62,11 @@ class HomeScreen extends Component {
     }
   }
 
- 
+
   componentWillMount () {
     // setup initial Categories if Redux exist
     this.setupCategories()
-    
+
   }
 
   componentWillReceiveProps (newProps) {
@@ -65,9 +74,11 @@ class HomeScreen extends Component {
     this.checkCategories(newProps)
   }
 
-  _handleStories (navigate) {
+  _handleCountryList (navigate) {
+    //Config.current_category_id=category_id
     navigate('CountryListScreen')
   }
+
   _handleClick (navigate, category_id, category_name) {
     //Config.current_category_id=category_id
     navigate('CategoriesDetailScreen', {category_id:category_id, category_name:category_name})
@@ -93,30 +104,43 @@ class HomeScreen extends Component {
       }
   }
 
+  // find City With id return String of the city name
+  findCityWithCityId(id){
+    if(id.toString() === "280"){
+      return "New York"
+    }else{
+      return "New Jersey"
+    }
+  }
+
   render () {
     const { navigate } = this.props.navigation
     return (
     <View style={{ flex:1 }}>
       <ScrollView>
         <View>
-          <HeaderComponents onPress={() => this._handleStories(navigate)} text="TOMATO" textColor={Colors.snow} iconColor={Colors.snow} ></HeaderComponents>
+          <HeaderComponents text="TOMATO" textColor={Colors.snow} onPress={()=>this._handleCountryList(navigate)} iconColor={Colors.snow} ></HeaderComponents>
         </View >
         <CustomActivityIndicator fetching={this.props.categoriesFetching}/>
         <View style={{margin:10}}>
-          <Picker
-            mode="dropdown"
-            headerBackButtonText="Back"
-            selectedValue={this.state.selected}
-            onValueChange={this.onValueChange.bind(this)}>
-            <Item label="New York" value="280" />
-            <Item label="New Jersey" value="3959" />
-          </Picker>
-          
-          <ListView 
-              dataSource = { this.state.dataSource } 
+          <View style={{flexDirection: 'row'}}>
+            <Picker
+              mode="dropdown"
+              iosHeader="Select one"
+              headerBackButtonText="Back"
+              selectedValue={this.state.selected}
+              onValueChange={this.onValueChange.bind(this)}>
+              <Item label="New York" value="280" />
+              <Item label="New Jersey" value="3959" />
+            </Picker>
+            <IconTwo name='caret-down'
+                     type='font-awesome' color={Colors.blue} />
+          </View>
+          <ListView
+              dataSource = { this.state.dataSource }
               renderRow = {this.renderRow.bind(this)}
               enableEmptySections ={true}
-              /> 
+              />
         </View>
         </ScrollView>
         <View>
@@ -140,7 +164,8 @@ const mapStateToProps = (state) => {
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    categoriesRequest: () => dispatch(TomatoActions.categoriesRequest())
+    categoriesRequest: () => dispatch(TomatoActions.categoriesRequest()),
+    switchEntity: (id) => dispatch(TomatoActions.switchEntity(id))
   }
 }
 
