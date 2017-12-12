58a9e0fa14ce842c4abd386d992aabf570b5f0bd
Laksmita <mithawid@gmail.com>
2017-12-12 09:30:15 +0700

    Update CountryListScreen.js

--- App/Containers/CountryListScreen.js
+++ App/Containers/CountryListScreen.js
@@ -5,34 +5,42 @@ import { Images, Colors, Metrics } from '../Themes'
 import { List, ListItem,Button } from 'react-native-elements'
 import HeaderCenterComponent from '../Components/HeaderCenterComponent'
 import style from '../Components/Styles/TomatoStyles'
+import {connect} from 'react-redux'
+import TomatoActions from '../Redux/TomatoRedux'
 
 const list = [
   {
-    name: 'USA',
+    name: 'NEW YORK',
     avatar_url: 'http://flaglane.com/download/american-flag/american-flag-large.png',
-    subtitle: 'Vice President'
+    id: "280",
   },
   {
     name: 'NEW JERSEY',
     avatar_url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a4/New_Jersey_State_Flag.svg/1200px-New_Jersey_State_Flag.svg.png',
-    subtitle: 'Vice Chairman'
+    id: "3959",
   }
 ]
 
 
 
-export default class CountryListScreen extends Component {
-    _handleStories (navigate) {
-        navigate('HomeScreen')
-      }
+class CountryListScreen extends Component {
+
+  _handleBack (navigate) {
+    navigate('HomeScreen')
+  }
+
+  handleItem(id) {
+    this.props.switchEntity(id)
+    this.props.navigation.navigate('HomeScreen')
+  }
 
   render () {
     const { navigate } = this.props.navigation
     return (
       <View>
         <View>
-            <HeaderCenterComponent  
-                text='Select City' 
+            <HeaderCenterComponent
+                text='Select City'
                 textColor={Colors.snow}>
             </HeaderCenterComponent>
         </View>
@@ -44,16 +52,34 @@ export default class CountryListScreen extends Component {
                 roundAvatar
                 avatar={{uri:l.avatar_url}}
                 key={i}
-                title={l.name}
+                title={l.name} onPress={()=>this.handleItem(l.id)}
             />
             ))
         }
-        </List>  
-        </View>  
+        </List>
+        </View>
         <View style={style.viewCountryList}>
-            <Button title="CANCEL" onPress={() => this._handleStories(navigate)} backgroundColor='#03A9F4'></Button>
-        </View>  
+            <Button title="CANCEL" onPress={() => this._handleBack(navigate)} backgroundColor='#03A9F4'></Button>
+        </View>
       </View>
     )
   }
 }
+
+
+CountryListScreen.propTypes = {}
+
+const mapStateToProps = (state) => {
+  return {
+    entity_id: state.tomato.entity_id
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    switchEntity: (id) => dispatch(TomatoActions.switchEntity(id))
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(CountryListScreen)
+
